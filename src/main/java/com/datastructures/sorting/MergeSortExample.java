e7c55a9464068797b3971160dc0cb1997d9383cc
Piyush Kumar <piyush.kumar@logmein.com>
2019-08-10 20:14:14 +0530

    fixing mergesort and adding iterative merge sort

--- src/main/java/com/datastructures/sorting/MergeSortExample.java
+++ src/main/java/com/datastructures/sorting/MergeSortExample.java
@@ -26,12 +26,10 @@ public class MergeSortExample {
     public static void merge(int[] arr, int start, int mid, int last) {
         int n1 = mid - start + 1 ;
         int n2 = last - mid;
-        int leftArray[] = new int[n1 + 1];
-        int rightArray[] = new int[n2 + 1];
-        leftArray[n1] = Integer.MAX_VALUE;
-        rightArray[n2] = Integer.MAX_VALUE;
+        int leftArray[] = new int[n1];
+        int rightArray[] = new int[n2];
 
-        for (int i = 0; i <= n1; i++) {
+        for (int i = 0; i < n1; i++) {
             leftArray[i] = arr[start + i];
         }
 
@@ -41,7 +39,8 @@ public class MergeSortExample {
 
         int j = 0;
         int k = 0;
-        for (int i = 0; i <= last; i++) {
+        int i = start;
+        for (; j < n1 && k < n2; i++) {
 
             if (j < n1 && leftArray[j] <= rightArray[k]) {
                 arr[i] = leftArray[j];
@@ -51,54 +50,26 @@ public class MergeSortExample {
                 k++;
             }
         }
+
+        while (j < n1){
+            arr[i] = leftArray[j];
+            i++;
+            j++;
+        }
+
+        while (k < n2){
+            arr[i] = rightArray[k];
+            i++;
+            k++;
+        }
     }
 
-//    public static void merge(int[] arr, int low, int mid, int last) {
-//
-//        int i = low;
-//        int j = mid + 1;
-//        int l = 0;
-//
-//        while (i <= mid && j <= last) {
-//            if (arr[i] <= arr[j]) {
-//                helper[l] = arr[low];
-//                i++;
-//                l++;
-//
-//            } else {
-//                helper[l] = arr[j];
-//                j++;
-//                l++;
-//            }
-//        }
-//
-//        if (i < mid) {
-//            while (i <= mid) {
-//                helper[l] = arr[i];
-//                i++;
-//                l++;
-//            }
-//        }
-//
-//        if (j < last) {
-//            while (j <= last) {
-//                helper[l] = arr[j];
-//                j++;
-//                l++;
-//            }
-//        }
-//    }
-
     public static void main(String[] args) {
-        int arr[] = {5, 3, 7,4,9};
-        //MergeSortExample sortExample = new MergeSortExample(arr.length);
+        int arr[] = {9, 3, 7,5,6,4,8,2};
         mergeSort(arr, 0, arr.length - 1);
         System.out.println("Printing sorted array :::: ");
         for (int i : arr){
             System.out.print(i + "\t");
         }
-//        for (int i : helper) {
-//            System.out.print(i);
-//        }
     }
 }
