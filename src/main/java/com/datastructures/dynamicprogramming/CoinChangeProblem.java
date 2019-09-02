8e1aa2ddf3e2ede6bc69fb40e3d861c181313173
Piyush Kumar <piyush.kumar@logmein.com>
2019-09-02 19:27:48 +0530

    DP problems - coin change problem and coin change variation i.e minimum number of coins

--- src/main/java/com/datastructures/dynamicprogramming/CoinChangeProblem.java
+++ src/main/java/com/datastructures/dynamicprogramming/CoinChangeProblem.java
@@ -2,6 +2,8 @@ package com.datastructures.dynamicprogramming;
 
 import org.apache.commons.lang.ArrayUtils;
 
+import java.util.Arrays;
+
 /**
  * Find the number of ways of arrangement of coins(can be different or same) to make the given sum.
  *
@@ -11,28 +13,86 @@ import org.apache.commons.lang.ArrayUtils;
 public class CoinChangeProblem {
 
     /* This is a brute force approach. It will work in exponential time complexity. */
-    static int getNumOfWaysToMakeSum(int coins[], int size, int value){
+    static int getNumOfWaysToMakeSum(int coins[], int size, int value) {
 
         /* Means no solution */
-        if (size < 0 && value >=1){
+        if (size < 0 && value >= 1) {
             return 0;
         }
 
         /* Means no solution */
-        if (value < 0){
+        if (value < 0) {
             return 0;
         }
 
-        if (value == 0){
+        if (value == 0) {
             return 1;
         }
 
-        return getNumOfWaysToMakeSum(coins, size-1, value) + getNumOfWaysToMakeSum(coins, size, value - coins[size]);
+        return getNumOfWaysToMakeSum(coins, size - 1, value) + getNumOfWaysToMakeSum(coins, size, value - coins[size]);
+    }
+
+
+    /* This solution will work in O(size*amount) time complexity and O(size*amount) space complexity. */
+    static int getNumOfWaysToMakeSumDPSol1(int coins[], int size, int value) {
+
+        int[][] solTable = new int[size + 1][value + 1];
+
+        for (int i = 0; i < solTable[0].length; i++) { // setting 1 in all the cells of 1st row
+            solTable[0][i] = 1;
+        }
+        for (int i = 0; i < solTable.length; i++) { // setting 1 in all the rows 1st column.
+            solTable[i][0] = 1;
+        }
+
+        for (int i = 1; i < solTable.length; i++) {
+            for (int j = 1; j < solTable[i].length; j++) {
+                if (coins[i] > j) { // when coin value is greater than actual value, then just copy the value from the above cell.
+                    solTable[i][j] = solTable[i - 1][j];
+
+                } else {
+                    solTable[i][j] = solTable[i][j - coins[i]] + solTable[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println("The solution table for 'getNumOfWaysToMakeSumDPSol1' is :: ");
+        for (int[] arr : solTable) {
+            System.out.println(Arrays.toString(arr));
+        }
+        return solTable[size][value];
+    }
+
+
+    /* Note : When your existing row in the DP Solution table(which is of two dimensional size) just makes use of the last
+     * row (no other previous rows than the last row), it is quite possible to change that solution with two dimensional array
+     * to one dimensional array as a part of further optimization of the space complexity.
+    * */
+    static int getNumOfWaysToMakeSumDPSol2(int coins[], int size, int value) {
+
+        int[] solTable = new int[value + 1];
+
+        for (int i = 0; i < solTable.length; i++) {
+
+            solTable[i] = 1;
+        }
+
+        for (int i = 1; i <= size; i++){
+            for (int j = 1; j <= value; j++){
+
+                if (coins[i] <= j){
+                    solTable[j] = solTable[j] + solTable[j - coins[i]];
+                }
+            }
+        }
+
+        System.out.println("The solution table for 'getNumOfWaysToMakeSumDPSol2' is :: " + Arrays.toString(solTable));
+        return solTable[value];
     }
 
 
     public static void main(String[] args) {
-        int coins[] = {1,2,3};
+        int coins[] = {1, 2, 3};
         int size = coins.length - 1;
         int value = 5;
 
@@ -40,6 +100,8 @@ public class CoinChangeProblem {
 
         System.out.println("The number of ways to make the sum is ::: " + numWays);
 
-    }
+        System.out.println("The number of ways to make the sum is ::: " + getNumOfWaysToMakeSumDPSol1(coins, size, value));
 
+        System.out.println("The number of ways to make the sum is ::: " + getNumOfWaysToMakeSumDPSol2(coins, size, value));
+    }
 }
